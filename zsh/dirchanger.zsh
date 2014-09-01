85e4bf6b6d439505123bb5e4c28ca21be28ba365
Riccardo Sven Risuleo <riccardosven@gmail.com>
2014-09-01 20:31:15 +0200

    Added Anaconda to PATH

--- zsh/dirchanger.zsh
+++ zsh/dirchanger.zsh
@@ -1,12 +1,26 @@
+echo "loaded dirstack!"
+
 DIRSTACKSIZE=9
 DIRSTACKFILE=~/.zdirs
 if [[ -f $DIRSTACKFILE ]] && $#dirstack -eq 0 ]]; then
 	dirstack=( ${(f)"$(< $DIRSTACKFILE)"} )
-	[[ -d $dirstack[1] ]] && cd $dirstack[1] #&& cd $OLDPWD
+	[[ -d $dirstack[1] ]] && cd $dirstack[1] && cd $OLDPWD
 fi
 
 cd $DIRSTACKFILE[1]
 
 chpwd() {
 	print -l $PWD ${(u)dirstack}> $DIRSTACKFILE
+}
+
+jump() {
+    i=1
+    for dir in $dirstack
+    do
+        print \[$i\] $dir
+        let i+=1
+    done
+    printf "jump to directory: "
+    read dir
+    cd $dirstack[$dir]
 }
